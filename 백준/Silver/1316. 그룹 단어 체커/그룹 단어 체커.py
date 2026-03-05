970be8eefe878be6db0483a20ca934ddeb80f77c
choihyun-1110 <74152226+choihyun-1110@users.noreply.github.com>
2026-03-05 14:57:18 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,38 +1,87 @@
+# import sys
+# input = sys.stdin.readline
+
+# line = int(input().strip()) # 갯수
+
+# l1 = []
+# cnt=0
+
+# # 추가
+# for i in range(line):
+#     word = input().strip()
+#     l1.append(word)
+# # l1 = ['aba','aann']
+# for i in l1:
+#     flag = True
+#     check = []
+#     s1 = set(check)
+#     prev = i[0]
+#     s1.add(prev)
+#     for j in range(1,len(i)): # 객체 하나의 길이
+#         cur = i[j]
+#         if prev != cur:
+#             if cur in s1:
+#                 flag = False
+#                 break
+#             else:
+#                 s1.add(cur)
+#         else:
+#             pass
+#         prev = cur
+#     if flag == True:
+#         cnt +=1
+#     else:
+#         flag = True
+# print(cnt)
+
+
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input().strip())
+# ans = 0
+
+# for _ in range(n):
+#     word = input().strip()
+#     seen = set()
+#     prev = ''
+
+#     ok = True
+#     for c in word:
+#         if c != prev:          # 새 블록 시작
+#             if c in seen:      # 전에 나온 문자가 다시 등장(끊겨서 재등장)
+#                 ok = False
+#                 break
+#             seen.add(c)
+#             prev = c           # 현재 블록 문자 갱신
+#         # else: 같은 문자 연속이면 그냥 진행
+
+#     if ok:
+#         ans += 1
+
+# print(ans)
+
+
 import sys
 input = sys.stdin.readline
-
-line = int(input().strip()) # 갯수
-
-l1 = []
+n= int(input().strip())
 cnt=0
 
-# 추가
-for i in range(line):
+for _ in range(n):
     word = input().strip()
-    l1.append(word)
-# l1 = ['aba','aann']
-for i in l1:
+    s1 =set()
+    prev = ''
     flag = True
-    check = []
-    s1 = set(check)
-    prev = i[0]
-    s1.add(prev)
-    for j in range(1,len(i)): # 객체 하나의 길이
-        cur = i[j]
-        if prev != cur:
-            if cur in s1:
+
+    for i in word:
+        if i !=prev:
+            if i in s1:
                 flag = False
                 break
-            else:
-                s1.add(cur)
-        else:
-            pass
-        prev = cur
-    if flag == True:
-        cnt +=1
-    else:
-        flag = True
+            s1.add(i)
+        prev = i
+    if flag:
+        cnt+=1
+
 print(cnt)
-
-
-
